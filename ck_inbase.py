a7d23ccd95b7a4ccbcb6bcc934b94009c86e4ac6
jiachao <jiachao_soochow@163.com>
2023-04-02 21:51:27 +0800

    调整为分段mapping

--- ck_inbase.py
+++ ck_inbase.py
@@ -25,10 +25,11 @@ conf=SparkSession.sparkContext._conf.setAll([('spark.executor.memory', '8g'),
 #lines=SparkSession.read.csv(r"E:\220914\steak\age-0307.txt")
 #lines.toDF("phonemd5",'age').createOrReplaceTempView('xjh_age_0307')
 #SparkSession.sql('''create table age.xjh_age_0307 as select phonemd5,age from xjh_age_0307''')
-
-#.此方法可以对列更名
+haoduan=['130','185', '136', '198', '131', '147', '148', '153', '146', '133', '187', '170', '157', '195', '132', '178', '156', '199', '162', '134', '145', '165', '183', '172', '137', '175', '184', '191', '189', '188', '139', '180', '193', '182', '177', '166', '138', '158', '159', '149', '173', '176', '196', '174', '135', '186', '152', '150', '151', '171', '167', '155', '181']
 SparkSession.sql("set hive.exec.dynamic.partition=true")
 SparkSession.sql("set hive.exec.dynamic.partition.mode=nonstrict")
-SparkSession.sql('''insert overwrite table chaojia.CK_PRODUCT_MONTH_WAY_phone
-partition(PRODUCT,month,way)
-select b.phone,a.PRODUCT,a.month,a.way from chaojia.CK_PRODUCT_MONTH_WAY_1 a left join chaojia.all_phone b on a.phonemd5=b.phonemd5 where b.phone is not null and a.product rlike 'ZAXD' ''')
+#.此方法可以对列更名
+for i in haoduan:
+    SparkSession.sql('''insert overwrite table chaojia.CK_PRODUCT_MONTH_WAY_phone
+partition(PRODUCT,month,way,haoduan)
+select b.phone,a.PRODUCT,a.month,a.way,b.haoduan from chaojia.CK_PRODUCT_MONTH_WAY_1 a left join chaojia.all_phone b on a.phonemd5=b.phonemd5 where b.phone is not null and b.haoduan='{}' '''.format(i))
